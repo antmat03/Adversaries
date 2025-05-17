46c714cff61b1aba5601ed9843c10990e36edff6
Resinanzz <114818509+Resinanzz@users.noreply.github.com>
2025-05-16 20:42:53 -0400

    fixed visual bug

--- src/main/java/org/resinanzz/adversaries/item/GauntletOfRageItem.java
+++ src/main/java/org/resinanzz/adversaries/item/GauntletOfRageItem.java
@@ -1,5 +1,8 @@
 package org.resinanzz.adversaries.item;
 
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
@@ -22,21 +25,21 @@ public class GauntletOfRageItem extends Item {
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         ItemStack itemstack = player.getItemInHand(hand);
         int energy = player.getData(AdversariesModAttachments.BLOOD_ENERGY);
-            player.swing(hand);
             if(energy >= 5){
                 if (!player.isCrouching()) {
                    // ((ServerLevel)player.level()).sendParticles(ParticleTypes.EXPLOSION,player.getX(),player.getY() ,player.getZ() , 1, 0.4,0.4,0.4, 0);
                     PunchProjectileEntity.shoot(level, player, level.getRandom(), 1, 2, 0d, 1f, 1);
                     EnergyHandler.addEnergy(player, -5, AdversariesModAttachments.BLOOD_ENERGY, AdversariesModAttachments.BLOOD_ENERGY_LIMIT);
-                    System.out.println(player.getData(AdversariesModAttachments.BLOOD_ENERGY));
-                    player.getCooldowns().addCooldown(this, 8);
+                    player.getCooldowns().addCooldown(this, 7);
+                    player.swing(hand);
                 }
             }
             if(energy >= 50 && player.isCrouching()){
                 EnergyHandler.addEnergy(player, -50, AdversariesModAttachments.BLOOD_ENERGY, AdversariesModAttachments.BLOOD_ENERGY_LIMIT);
                 PunchVolley.doVolley(level, player, level.getRandom(), 1, 2, 0d, 1f, 1.3f, 50);
+                level.playSound(player, player.getX(), player.getY(), player.getZ(), BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("adversaries:monster_growl")), SoundSource.PLAYERS, (float) (1.3-0.9f), 1f);
+                player.swing(hand);
             }
-
 
         player.awardStat(Stats.ITEM_USED.get(this));
         return InteractionResultHolder.sidedSuccess(itemstack, level.isClientSide());
