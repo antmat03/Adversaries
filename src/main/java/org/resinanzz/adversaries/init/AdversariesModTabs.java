10a3d21c28fabce765a4553b26ad604055dc787f
Resinanzz <114818509+Resinanzz@users.noreply.github.com>
2025-05-18 00:17:20 -0400

    renamed faye food + added renamed class

--- src/main/java/org/resinanzz/adversaries/init/AdversariesModTabs.java
+++ src/main/java/org/resinanzz/adversaries/init/AdversariesModTabs.java
@@ -38,7 +38,7 @@ public class AdversariesModTabs {
                 tabData.accept(AdversariesModItems.INFINITE_DEATH.get());
                 tabData.accept(AdversariesModItems.PURIFIED_FLESH.get());
                 tabData.accept(AdversariesModItems.DWELLER_HEART.get());
-                tabData.accept(AdversariesModItems.FREYAN_DUST.get());
+                tabData.accept(AdversariesModItems.EIK_FAT.get());
                 tabData.accept(AdversariesModItems.GLOWSTONE_CORE.get());
                 tabData.accept(AdversariesModItems.REACTIVE_STRIP.get());
                 tabData.accept(AdversariesModItems.LAMBDAMETER.get());
