0fe0d492391acbc2adfce600cd508d8adf704b52
Resinanzz <114818509+Resinanzz@users.noreply.github.com>
2025-05-18 00:05:32 -0400

    cleaned imports

--- src/main/java/org/resinanzz/adversaries/item/InfiniteDeathItem.java
+++ src/main/java/org/resinanzz/adversaries/item/InfiniteDeathItem.java
@@ -1,18 +1,12 @@
 package org.resinanzz.adversaries.item;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.Level;
-import org.resinanzz.adversaries.Adversaries;
-import org.resinanzz.adversaries.component.AdversariesModAttachments;
+import org.resinanzz.adversaries.init.AdversariesModAttachments;
 import org.resinanzz.adversaries.component.EnergyHandler;
 
 public class InfiniteDeathItem extends AbstractTalismanItem{
